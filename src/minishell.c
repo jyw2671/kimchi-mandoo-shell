6088cb56dab5af449acb1b0b6daa2b8ec912d2db
jaeskim <jaeskim@student.42seoul.kr>
2021-04-14 20:22:54 +0900

    fix: init_envp
    return type chagne, env[last] = null
    
    issues: #21

--- src/minishell.c
+++ src/minishell.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yjung <yjung@student.42seoul.kr>           +#+  +:+       +#+        */
+/*   By: jaeskim <jaeskim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/14 19:40:29 by yjung             #+#    #+#             */
-/*   Updated: 2021/04/14 19:49:03 by yjung            ###   ########.fr       */
+/*   Updated: 2021/04/14 20:22:45 by jaeskim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,30 +24,33 @@ static int	envp_len(char **envp)
 	return (cnt);
 }
 
-static void	init_envp(int argc, char *argv[], char *envp[], char **env)
+static char	**init_envp(int argc, char *argv[], char *envp[])
 {
-	int	i;
+	int		i;
+	char	**env;
 
 	(void)argc;
 	(void)argv;
 	env = (char **)ft_calloc(sizeof(char *), (envp_len(envp) + 1));
 	if (!env)
-		exit_shell();
+		exit(1);
 	i = -1;
 	while (envp[++i])
 	{
 		env[i] = ft_strdup(envp[i]);
-		// if (!env[i])
-		// 	exit_shell();
+		if (!env[i])
+			exit(1);
 	}
+	env[i] = 0;
+	return (env);
 }
 
 int	main(int argc, char *argv[], char *envp[])
 {
 	char	**env;
 
-	init_envp(argc, argv, envp, &env);
+	env = init_envp(argc, argv, envp);
 	printf("minishell!\n");
-	exec_cmd(parse_cmd(argv[argc - 1]), envp);
+	exec_cmd(parse_cmd(argv[argc - 1]), env);
 	return (0);
 }
