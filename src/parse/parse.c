f15ad728c2335754a4bd678a439430760bedc38f
JaeSeoKim <devjaeseo@gmail.com>
2021-04-16 20:58:49 +0900

    feat: add tokenizer
    기본적인 tokenizer 함수 추가 및 테스트 코드 추가!

--- src/parse/parse.c
+++ src/parse/parse.c
@@ -6,14 +6,31 @@
 /*   By: jaeskim <jaeskim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/14 13:39:46 by jaeskim           #+#    #+#             */
-/*   Updated: 2021/04/16 19:59:12 by jaeskim          ###   ########.fr       */
+/*   Updated: 2021/04/16 20:57:25 by jaeskim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+#include "parse_util.h"
 
-t_list	*parse_line(char *cmd)
+t_list	*parse_line(char *line)
 {
-	printf("parse_cmd! : %s\n", cmd);
+	int		i;
+	char	**tokens;
+
+	printf("%sCMD: %s%s%s\n", TEXT_COLOR, VALUE_COLOR, line, NO_COLOR);
+	printf("%s=============================================================%s\n",
+		TEXT_COLOR, NO_COLOR);
+	tokens = tokenizer(line);
+	printf("[ ");
+	i = 0;
+	while (tokens[i])
+	{
+		printf("%s\"%s%s%s\"%s, ", \
+			TEXT_COLOR, VALUE_COLOR, tokens[i], TEXT_COLOR, NO_COLOR);
+		free(tokens[++i]);
+	}
+	printf("]\n");
+	free(tokens);
 	return (0);
 }
