ddfd409845be61baeb08f84565901fd99652ad3e
JaeSeoKim <devjaeseo@gmail.com>
2021-04-16 21:51:25 +0900

    remove git conflict code

--- include/parse.h
+++ include/parse.h
@@ -6,11 +6,7 @@
 /*   By: jaeskim <jaeskim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/14 13:41:05 by jaeskim           #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2021/04/16 19:58:35 by jaeskim          ###   ########.fr       */
-=======
 /*   Updated: 2021/04/16 21:39:58 by jaeskim          ###   ########.fr       */
->>>>>>> 1b36cd8... refactor: remove parse_args, update t_cmd
 /*                                                                            */
 /* ************************************************************************** */
 
