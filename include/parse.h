075735efdd6f760fbc3b9bb972b98036c0ea20b5
JaeSeoKim <devjaeseo@gmail.com>
2021-04-16 21:40:56 +0900

    refactor: remove parse_args, update t_cmd
    내부에서 작동하는 로직 변경!
    
    issues: #6 #7

--- include/parse.h
+++ include/parse.h
@@ -6,7 +6,11 @@
 /*   By: jaeskim <jaeskim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/14 13:41:05 by jaeskim           #+#    #+#             */
+<<<<<<< HEAD
 /*   Updated: 2021/04/16 19:58:35 by jaeskim          ###   ########.fr       */
+=======
+/*   Updated: 2021/04/16 21:39:58 by jaeskim          ###   ########.fr       */
+>>>>>>> 1b36cd8... refactor: remove parse_args, update t_cmd
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +21,6 @@
 
 t_list	*parse_line(char *line);
 char	*parse_cmd(char *cmd, char **envp);
-char	**parse_args(char *args, char **envp);
 char	*parse_arg(char *arg, char **envp);
 
 #endif
